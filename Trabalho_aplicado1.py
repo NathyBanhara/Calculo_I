f0033a03807ba6886f2a0a798f5844c985d15978
Natália Banhara <90213076+NathyBanhara@users.noreply.github.com>
2021-12-15 22:32:34 -0300

    file

---
+++ Trabalho_aplicado1.py
@@ -0,0 +1,45 @@
+def equacao(x):
+    e = 2.718281828459045235360287
+    return e**(-x) - 2*(x**2) + 4
+
+def sinais(x):
+    if x > 0:
+        return True
+    return False
+
+def maior(x, y):
+    if x > y:
+        return x
+    return y
+
+while True:
+    x, y = map(float, input().split())
+    f_x = equacao(x)
+    f_y = equacao(y)
+    sinal_x = sinais(f_x)
+    sinal_y = sinais(f_y)
+    if sinal_x == sinal_y:
+        print("não é possível afirmar que existe solução neste intervalo, tente outros dois números")
+    else:
+        while abs(x-y) > 0.1:
+            novo = (x+y)/2
+            f_novo = equacao(novo)
+            sinal_novo = sinais(f_novo)
+            if sinal_novo == sinal_x:
+                f_x = f_novo           
+                x = novo
+            else:
+                f_y = f_novo
+                y = novo
+        aux = maior(x, y)
+        if aux == x:
+            novo = y
+            y = x
+            x = novo
+        print(f"[{x}, {y}]")
+        break
+
+                
+    
+
+
